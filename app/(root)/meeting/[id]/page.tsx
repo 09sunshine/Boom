7748004c08d7690fd4683dd52b16e2bda46d7a6d
09sunshine <sanshey230456@aitpune.edu.in>
2025-07-30 01:13:18 +0530

    sidebar

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Meeting = async ({params} : {params: {id:string}}) => {
+  return (
+    <div>
+      Meeting Room: #{params.id}
+    </div>
+  )
+}
+
+export default Meeting
